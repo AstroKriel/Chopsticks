29c8660041db8d62b2f5814383358e2adaed829e
neco kriel <necokriel@gmail.com>
2022-06-06 09:43:36 +1000

    fixed game state generation (TODO: fix branch hierarchy/depth)

--- game_states.py
+++ game_states.py
@@ -1,3 +1,4 @@
+## https://github.com/joaossmacedo/TicTacToe/blob/master/Tree.py
 
 class State():
   def __init__(self, hands: list, depth: int):
@@ -10,18 +11,34 @@ def listNextHands(
     apl: int, apr: int,
     wpl: int, wpr: int
   ):
-  ap_hand = [apl, apr] # active player
+  # import pdb; pdb.set_trace() # for DEBUGGING
+  ap_hands = [apl, apr] # active player
   ## check if either of the two players have the same number of fingers on both their hands
   num_hands_to_attack_with = 1 if (apl == apr) else 2
   num_hands_to_attack      = 1 if (wpl == wpr) else 2
-  ## list the next possible hands
+  ## store the next possible hands
   for ap_hand_index in range(num_hands_to_attack_with):
+    ## player can't attack with a hand that has zero fingers
+    if ap_hands[ap_hand_index] == 0:
+      continue
     for wp_hand_index in range(num_hands_to_attack):
-      wp_hand = [wpl, wpr] # waiting player
-      wp_hand[wp_hand_index] += ap_hand[ap_hand_index]
-      state.list_next_hands.append(
-        State(ap_hand + wp_hand, state.depth+1)
-      )
+      wp_hands = [wpl, wpr] # waiting player
+      ## attacking player can't add to a hand that has zero fingers
+      if wp_hands[wp_hand_index] == 0:
+        continue
+      wp_hands[wp_hand_index] = (wp_hands[wp_hand_index] + ap_hands[ap_hand_index]) % 5
+      if state.depth % 2 == 0:
+        ## player 1 -> 2
+        state.list_next_hands.append(
+          State(ap_hands + wp_hands, state.depth+1)
+        )
+      else:
+        ## player 2 -> 1
+        state.list_next_hands.append(
+          State(wp_hands + ap_hands, state.depth+1)
+        )
+
+## TODO: bug need to safely store a copy of the parent node's depth
 
 class GameTree():
   def __init__(self):
@@ -31,7 +48,12 @@ class GameTree():
     self.root = State([1,1,1,1], 0)
     self.__auxSimulate(self.root)
   def __auxSimulate(self, state: State):
-    ## extract player hands
+    print(
+      "P~: " if state.depth == 0 else "P1: " if state.depth % 2 == 1 else "P2: ",
+      state.hands,
+      state.depth
+    )
+    ## extract player hands and apply game rules
     p1l = state.hands[0]
     p1r = state.hands[1]
     p2l = state.hands[2]
@@ -46,9 +68,9 @@ class GameTree():
       return
     ## identify all next possible moves
     ## player 1 -> 2
-    if state.depth % 2 == 1:
+    if state.depth % 2 == 0:
       listNextHands(state, apl=p1l, apr=p1r, wpl=p2l, wpr=p2r)
-    ## player 2-> 1
+    ## player 2 -> 1
     else: listNextHands(state, apl=p2l, apr=p2r, wpl=p1l, wpr=p1r)
     ## progress tree traversal and evaluate those possible options
     for branch_index in range(len(state.hands)):
@@ -57,7 +79,6 @@ class GameTree():
 def main():
   game = GameTree()
   game.simulate()
-
 
 if __name__ == "__main__":
   main()
