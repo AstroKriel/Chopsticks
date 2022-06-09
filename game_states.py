c596f8920ed674bb3fa9daaf7c5628c75d2a8218
neco kriel <necokriel@gmail.com>
2022-06-09 10:10:23 +1000

    update: fixed node hierarchy

--- game_states.py
+++ game_states.py
@@ -1,13 +1,9 @@
 ## https://github.com/joaossmacedo/TicTacToe/blob/master/Tree.py
+from anytree import Node, RenderTree
 
-class State():
-  def __init__(self, hands: list, depth: int):
-    self.hands = hands
-    self.depth = depth
-    self.list_next_hands = []
-
-def listNextHands(
-    state: State,
+def getNextStates(
+    node: Node,
+    depth: int,
     apl: int, apr: int,
     wpl: int, wpr: int
   ):
@@ -16,7 +12,8 @@ def listNextHands(
   ## check if either of the two players have the same number of fingers on both their hands
   num_hands_to_attack_with = 1 if (apl == apr) else 2
   num_hands_to_attack      = 1 if (wpl == wpr) else 2
-  ## store the next possible hands
+  ## create a list of the next possible hands
+  list_next_nodes = []
   for ap_hand_index in range(num_hands_to_attack_with):
     ## player can't attack with a hand that has zero fingers
     if ap_hands[ap_hand_index] == 0:
@@ -27,58 +24,90 @@ def listNextHands(
       if wp_hands[wp_hand_index] == 0:
         continue
       wp_hands[wp_hand_index] = (wp_hands[wp_hand_index] + ap_hands[ap_hand_index]) % 5
-      if state.depth % 2 == 0:
+      if depth % 2 == 0:
         ## player 1 -> 2
-        state.list_next_hands.append(
-          State(ap_hands + wp_hands, state.depth+1)
+        list_next_nodes.append(
+          Node(
+            (ap_hands + wp_hands),
+            parent = node
+          )
         )
       else:
         ## player 2 -> 1
-        state.list_next_hands.append(
-          State(wp_hands + ap_hands, state.depth+1)
+        list_next_nodes.append(
+          Node(
+            (wp_hands + ap_hands),
+            parent = node
+          )
         )
-
-## TODO: bug need to safely store a copy of the parent node's depth
+  return list_next_nodes
 
 class GameTree():
   def __init__(self):
     self.root = None
     self.tree_height = 0
+  def printTree(self):
+    for pre, fill, node in RenderTree(self.root):
+      print("{}{}".format(pre, node.name))
   def simulate(self):
-    self.root = State([1,1,1,1], 0)
-    self.__auxSimulate(self.root)
-  def __auxSimulate(self, state: State):
-    print(
-      "P~: " if state.depth == 0 else "P1: " if state.depth % 2 == 1 else "P2: ",
-      state.hands,
-      state.depth
-    )
+    self.root = Node([1,1,1,1])
+    self.__auxSimulate(self.root, 0)
+  def __auxSimulate(
+      self,
+      node: Node,
+      depth: int
+    ):
+    # print("P{}: {} {}".format(
+    #   "~" if depth == 0 else "1" if depth % 2 == 1 else "2",
+    #   node.name, depth
+    # ))
     ## extract player hands and apply game rules
-    p1l = state.hands[0]
-    p1r = state.hands[1]
-    p2l = state.hands[2]
-    p2r = state.hands[3]
+    p1l = node.name[0]
+    p1r = node.name[1]
+    p2l = node.name[2]
+    p2r = node.name[3]
     ## check end game requirements
     bool_p1_lost = (p1l == 0) and (p1r == 0)
     bool_p2_lost = (p2l == 0) and (p2r == 0)
     if bool_p1_lost or bool_p2_lost:
       return
-    ## trim branches
-    if state.depth > 100:
+    ## trim branches at a particular depth
+    if depth >= MAX_DEPTH:
       return
     ## identify all next possible moves
     ## player 1 -> 2
-    if state.depth % 2 == 0:
-      listNextHands(state, apl=p1l, apr=p1r, wpl=p2l, wpr=p2r)
+    if depth % 2 == 0:
+      list_next_nodes = getNextStates(
+        node  = node,
+        apl   = p1l,
+        apr   = p1r,
+        wpl   = p2l,
+        wpr   = p2r,
+        depth = depth
+      )
     ## player 2 -> 1
-    else: listNextHands(state, apl=p2l, apr=p2r, wpl=p1l, wpr=p1r)
+    else:
+      list_next_nodes = getNextStates(
+        node  = node,
+        apl   = p2l,
+        apr   = p2r,
+        wpl   = p1l,
+        wpr   = p1r,
+        depth = depth
+      )
     ## progress tree traversal and evaluate those possible options
-    for branch_index in range(len(state.hands)):
-      self.__auxSimulate(state.list_next_hands[branch_index])
+    for branch_index in range(len(list_next_nodes)):
+      self.__auxSimulate(
+        node  = list_next_nodes[branch_index],
+        depth = depth+1
+      )
+
+MAX_DEPTH = 10
 
 def main():
   game = GameTree()
   game.simulate()
+  game.printTree()
 
 if __name__ == "__main__":
   main()
